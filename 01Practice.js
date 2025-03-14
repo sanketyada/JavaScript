03bc3e3f0f60e01f94ef8dbefc45d78d2b4186ce
sanket Yadav <yadavsanket663@gmail.com>
2025-03-14 11:08:21 +0530

    day 9 Practice

---
+++ 01Practice.js
@@ -0,0 +1,21 @@
+//DOM -->Document Object Model
+
+//The tree representation of HTML Code in Logical way know as DOM
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//   <h1>Snaket</h1>
+//   <p>lorem</p>
+// </body>
+// </html>
+
+//Logical Tree
+//Window-->documwnt-->head and Body
+//head-->meta and title
+//Body-->H1 ,p
---
+++ 02Practice.js
@@ -0,0 +1,9 @@
+// //Window 
+
+// window is global Object which is parent of document (HTML) there are multiple
+// properties window object have that programer according to theire needs
+
+console.log(window) //check in browser
+//Note in Node js there is not window object.
+
+console.log(document)//Current Html page codeknoe as document
